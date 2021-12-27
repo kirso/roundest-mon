9319fdbe72f19fe339e45b5655d64b4531a74c61
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2021-12-27 23:04:19 +0800

    :art: UI structure
    basic UI structure created

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,15 @@ import Image from "next/image"
 const Home: NextPage = () => {
 	return (
 		<div>
-			<h1 className="text-2xl">Hello</h1>
+			<div className="h-screen w-screen flex flex-col justify-center items-center">
+				<div className=" text-2xl text-center">Which Pokémon is rounder? </div>
+				<div className="p-4"></div>
+				<div className="border rounded p-8 flex justify-between max-w-2xl">
+					<div className="w-16 bg-red-200"></div>
+					<div className="p-8">VS</div>
+					<div className="w-16 bg-red-200"></div>
+				</div>
+			</div>
 		</div>
 	)
 }
