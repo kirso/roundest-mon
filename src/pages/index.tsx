7d3275bb4cbe12a7aeea67862a0eb0959ac88e46
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2022-01-16 23:14:09 +0800

    get random pokemon id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,19 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 import { trpc } from "@/utils/trpc"
+import { getOptionsForVote } from "@/utils/getRandomPokemon"
 
 const Home: NextPage = () => {
-	const { data, isLoading } = trpc.useQuery(["hello", { text: "Kirill" }])
-	if (isLoading) return <div>Loading...</div>
-
-	if (data) return <div>{data.greeting}</div>
+	const [first, second] = getOptionsForVote()
 	return (
 		<div>
 			<div className="h-screen w-screen flex flex-col justify-center items-center">
 				<div className=" text-2xl text-center">Which Pokémon is rounder? </div>
 				<div className="p-4"></div>
 				<div className="border rounded p-8 flex justify-between max-w-2xl">
-					<div className="w-16 bg-red-200"></div>
+					<div className="w-16 bg-red-800">{first}</div>
 					<div className="p-8">VS</div>
-					<div className="w-16 bg-red-200"></div>
+					<div className="w-16 bg-red-800">{second}</div>
 				</div>
 			</div>
 		</div>
